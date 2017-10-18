22a236b33dad316ce60b06d298b7aa887d0ba2ee
thai01255nt <32845101+thai01255nt@users.noreply.github.com>
2017-10-19 02:19:27 +0700

    Update driver_rover.py

--- driver_rover.py
+++ driver_rover.py
@@ -60,7 +60,7 @@ class RoverState():
         # of navigable terrain pixels.  This is a very crude form of knowing
         # when you can keep going and when you should stop.  Feel free to
         # get creative in adding new fields or modifying these!
-        self.stop_forward = 200 # Threshold to initiate stopping
+        self.stop_forward = 400 # Threshold to initiate stopping
         self.go_forward = 500 # Threshold to go forward again
         self.max_vel = 3 # Maximum velocity (meters/second)
         # Image output from perception step
